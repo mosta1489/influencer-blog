fee2991061069eccca51c1989c43b0d0ecde03c6
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-05-05 09:00:32 +0200

    .

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// if (process.env.NODE_ENV !== "production") {
-// }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 // ========= import required modules =====================
 const express = require("express");
@@ -9,8 +10,6 @@ const path = require("path");
 // require config ;
 process.env.SUPPRESS_NO_CONFIG_WARNING = "y";
 const config = require("config");
-
-require("dotenv").config();
 
 const fs = require("fs");
 const fileUpload = require("express-fileupload");
