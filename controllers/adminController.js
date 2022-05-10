f65b566a7826e1e68b8cc96e50011a7519756aef
Mohamed Hassan <zmlkawy67@gmail.com>
2022-05-10 21:07:21 +0200

    eidt

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,9 +5,13 @@ const fs = require("fs");
 
 async function getAdminPage(req, res) {
   res.render("admin", {
+    error: req.flash("error")[0],
+    success: req.flash("success")[0],
     isAdmin: req.session.isAdmin,
     isLoggedIn: req.session.userId,
+    userName: req.session.userName,
     fullName: req.session.fullName,
+    userImage: req.session.userImage,
   });
 }
 
